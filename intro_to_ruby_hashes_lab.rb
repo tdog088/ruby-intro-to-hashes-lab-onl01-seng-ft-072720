d40c1fccea35a109c2422a5114b6f56200734b43
Taliyah Wilson <taliyahwilson27@gmail.com>
2020-08-06 12:10:37 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -11,12 +11,12 @@ def monopoly
 	monopoly = {
 	  :railroads => 
 	  {
-	    :pieces => 4,
-	    :rent_in_dollars => {
-	      :one_piece_owned => 25,
-	      :two_pieces_owned => 50,
-	      :three_pieces_owned => 100,
-	      :four_pieces_owned => 200
+	  #  :pieces => 4,
+	  #  :rent_in_dollars => {
+	   #   :one_piece_owned => 25,
+	    #  :two_pieces_owned => 50,
+	    #  :three_pieces_owned => 100,
+	   #   :four_pieces_owned => 200
 	    },
 	    :names => 
 	    {
