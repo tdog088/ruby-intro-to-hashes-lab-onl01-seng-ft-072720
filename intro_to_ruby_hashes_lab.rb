6dca1192c17c899a0967e566082e64d5b42f7a4c
Taliyah Wilson <taliyahwilson27@gmail.com>
2020-08-06 12:45:15 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -67,11 +67,10 @@ def monopoly_with_third_tier
 	    :names => {
 	      :reading_railroad => {},
 	      :pennsylvania_railroad => {},
-	       :b_and_o_railroad => {},
-	        :shortline => {}
-	       
+	      :b_and_o_railroad => {},
+	      :shortline => {}
 	    }
-	  }
+	   }
     }
 end
 
