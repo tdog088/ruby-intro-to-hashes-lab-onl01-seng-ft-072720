a2850491df64adcb94c85a59c3db2c086f069288
Taliyah Wilson <taliyahwilson27@gmail.com>
2020-08-06 12:35:59 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -17,7 +17,7 @@ def monopoly
 	      #:two_pieces_owned => 50,
 	      #:three_pieces_owned => 100,
 	      #:four_pieces_owned => 200
-	    },
+	    }
 	    #:names => 
 	   #{
 	     # :reading_railroad => 
